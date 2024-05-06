739f4c346053af6b0c3704504a0da0dc2a9e7ae3
Gaurav Singh <gauravkalakoti19@gmail.com>
2024-05-06 22:37:21 +0530

    Update CreateTodo.jsx

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -22,7 +22,7 @@ const CreateTodo = () => {
             <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mb-10 hover:scale-105"
                 onClick={() => {
                     //use axios here for better code
-                    fetch("http://localhost:3000/todo",{
+                    fetch("https://todo-app-bell.onrender.com/todo",{
                         method: "POST",
                         body: JSON.stringify({
                             title: title,
