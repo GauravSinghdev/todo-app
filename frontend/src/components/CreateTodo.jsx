f491343239a5481db5149b134fd8c0625e97a08a
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-05-11 23:00:32 +0530

    edited deleted button

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -21,7 +21,7 @@ const CreateTodo = () => {
                     }}/>
                 <br />
 
-            <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-semibold rounded-lg px-5 py-2.5 mx-4 dark:bg-blue-600 dark:hover:bg-blue-700 text-xl focus:outline-none dark:focus:ring-blue-800 mb-10 hover:scale-105 h-[50px] "
+            <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-semibold rounded-lg px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 text-xl focus:outline-none dark:focus:ring-blue-800 mb-10 hover:scale-105 h-[50px] "
                 onClick={() => {
                     //use axios here for better code
                     fetch("https://todo-app-bell.onrender.com/todo",{
