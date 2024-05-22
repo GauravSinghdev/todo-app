d5c800c1312b32d3664cfbca3992fa82e5ecc9c7
Gaurav Singh <gauravkalakoti19@gmail.com>
2024-05-22 10:38:04 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URL);
 
 const todoSchema = mongoose.Schema({
     title: String,
